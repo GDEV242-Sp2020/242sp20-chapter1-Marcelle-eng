ed52721ca11853261d11e771e9f6ed10853b3dbf
Marcelle-eng <54851333+Marcelle-eng@users.noreply.github.com>
2020-02-03 10:29:41 -0500

    Update Picture.java
    comment author

--- Picture.java
+++ Picture.java
@@ -6,8 +6,8 @@
  *
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author  Marcelle Tamegnon
+ 
  */
 public class Picture
 {
